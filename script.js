99a47f7220af82e38c84c4160d4c6ea04bca17b7
Jackson Hayward <jchayward1@gmail.com>
2022-06-17 13:29:25 -0700

    Complete div grid

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
-let dimension = 2;
+let dimension = 8;
 
 function makeGrid(dimension) {
     const container = document.querySelector('.container');
+    let gridWidth = ""
     for (let i = 0; i < dimension * dimension; i++) {
         const div = document.createElement('div');
         div.textContent = `${i}`;
         container.appendChild(div)
-        console.log(i)
     }
+
+    for (let i = 0; i < dimension; i++) {
+        gridWidth += 'auto '
+    }
+    console.log(gridWidth)
+    container.style.gridTemplateColumns = gridWidth;
 }
 
 makeGrid(dimension)
